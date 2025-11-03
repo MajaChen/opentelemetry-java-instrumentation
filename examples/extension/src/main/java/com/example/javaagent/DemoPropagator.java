061ea50a559c7c6f4ebb8f65083e6f4012fcdca8
chenxinyuan <chenxinyuan@idea.edu.cn>
2025-11-03 16:10:00 +0800

    feat: 改造 DemoPropagator 使其能够传递自定义 http header

--- examples/extension/src/main/java/com/example/javaagent/DemoPropagator.java
+++ examples/extension/src/main/java/com/example/javaagent/DemoPropagator.java
@@ -10,7 +10,8 @@ import io.opentelemetry.context.ContextKey;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.context.propagation.TextMapSetter;
-import java.util.Collections;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,34 +19,44 @@ import java.util.List;
  * href="https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/context/api-propagators.md">
  * OpenTelemetry Specification</a> for more information about Propagators.
  *
+ * @author LASER
  * @see DemoPropagatorProvider
  */
 public class DemoPropagator implements TextMapPropagator {
-  private static final String FIELD = "X-demo-field";
-  private static final ContextKey<Long> PROPAGATION_START_KEY =
-      ContextKey.named("propagation.start");
+  private static final String X_REQUEST_ID_FIELD = "x-request-id";
+  private static final String X_OT_SPAN_CONTEXT_FIELD = "x-ot-span-context";
+  private static final ContextKey<String> REQUEST_ID_CONTEXT_KEY =
+      ContextKey.named("x.request.id");
+  private static final ContextKey<String> OT_SPAN_CONTEXT_KEY =
+      ContextKey.named("x.ot.span.context");
 
   @Override
   public List<String> fields() {
-    return Collections.singletonList(FIELD);
+    return Arrays.asList(X_REQUEST_ID_FIELD, X_OT_SPAN_CONTEXT_FIELD);
   }
 
   @Override
-  public <C> void inject(Context context, C carrier, TextMapSetter<C> setter) {
-    Long propagationStart = context.get(PROPAGATION_START_KEY);
-    if (propagationStart == null) {
-      propagationStart = System.currentTimeMillis();
+  public <C> void inject(Context context, C carrier, @NonNull TextMapSetter<C> setter) {
+    String requestId = context.get(REQUEST_ID_CONTEXT_KEY);
+    String otSpanContext = context.get(OT_SPAN_CONTEXT_KEY);
+    if (requestId != null) {
+      setter.set(carrier, X_REQUEST_ID_FIELD, requestId);
     }
-    setter.set(carrier, FIELD, String.valueOf(propagationStart));
+    if (otSpanContext != null) {
+      setter.set(carrier, X_OT_SPAN_CONTEXT_FIELD, otSpanContext);
+    }
   }
 
   @Override
-  public <C> Context extract(Context context, C carrier, TextMapGetter<C> getter) {
-    String propagationStart = getter.get(carrier, FIELD);
-    if (propagationStart != null) {
-      return context.with(PROPAGATION_START_KEY, Long.valueOf(propagationStart));
-    } else {
-      return context;
+  public <C> Context extract(@NonNull Context context, C carrier, TextMapGetter<C> getter) {
+    String requestId = getter.get(carrier, X_REQUEST_ID_FIELD);
+    String otSpanContext = getter.get(carrier, X_OT_SPAN_CONTEXT_FIELD);
+    if (requestId != null) {
+      context = context.with(REQUEST_ID_CONTEXT_KEY, requestId);
     }
+    if (otSpanContext != null) {
+      context = context.with(OT_SPAN_CONTEXT_KEY, otSpanContext);
+    }
+    return context;
   }
 }
--- examples/extension/src/main/java/com/example/javaagent/DemoPropagatorProvider.java
+++ examples/extension/src/main/java/com/example/javaagent/DemoPropagatorProvider.java
@@ -9,22 +9,24 @@ import com.google.auto.service.AutoService;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigurablePropagatorProvider;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  * Registers the custom propagator used by this example.
  *
+ * @author LASER
  * @see ConfigurablePropagatorProvider
  * @see DemoPropagator
  */
 @AutoService(ConfigurablePropagatorProvider.class)
 public class DemoPropagatorProvider implements ConfigurablePropagatorProvider {
   @Override
-  public TextMapPropagator getPropagator(ConfigProperties config) {
+  public TextMapPropagator getPropagator(@NonNull ConfigProperties config) {
     return new DemoPropagator();
   }
 
   @Override
   public String getName() {
-    return "demo";
+    return "ssf-customized-propagator";
   }
 }
